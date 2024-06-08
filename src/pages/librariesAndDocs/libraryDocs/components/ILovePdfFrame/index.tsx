d9b3dfb58f992f41c61dd868915784df2ca5fe25
Sallam Rady <salamrady@gmail.com>
2024-06-08 14:06:43 +0300

    handle download file

--- src/pages/librariesAndDocs/libraryDocs/components/ILovePdfFrame/index.tsx
+++ src/pages/librariesAndDocs/libraryDocs/components/ILovePdfFrame/index.tsx
@@ -4,6 +4,10 @@ import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import CompressIcon from "@mui/icons-material/Compress";
 import { LibraryDocumentionContext } from "../../context/LibraryDocumentionContext";
 import ConfirmFileProccess from "../ConfirmFileProccess";
+import GetAppOutlinedIcon from "@mui/icons-material/GetAppOutlined";
+import axios from "axios";
+import { Api } from "../../../../../constants";
+import { DocumentationFileType } from "../../../../../types/librariesAndDocs/DocumentationFile";
 
 export default function ILovePdfFrameIndex(props: PropsType) {
   // TODO::declare and define component state and variables
@@ -11,14 +15,27 @@ export default function ILovePdfFrameIndex(props: PropsType) {
   const [operationType, setOperationType] = useState<"Merge" | "Compress">(
     "Merge"
   );
-  const { activeFileToShow, selectedFilesIds } = useContext(
-    LibraryDocumentionContext
-  );
+  const { activeFileToShow, selectedFilesIds, handleSetActiveFile } =
+    useContext(LibraryDocumentionContext);
 
   // TODO::declare and define helper methods
   const handleClick = (type: "Merge" | "Compress") => {
     setOperationType(type);
     setOpenConfirmDialog(true);
+  };
+
+  const handleDownloadFile = () => {
+    if (activeFileToShow?.media?.[0]?.original_url) {
+      axios
+        .get<{ file: DocumentationFileType }>(
+          Api(`employee/library/file/show/${activeFileToShow.id}`)
+        )
+        .then((response) => {
+          handleSetActiveFile(response.data.file);
+          window.open(activeFileToShow?.media?.[0]?.original_url, "_blank");
+        })
+        .catch((err) => {});
+    }
   };
 
   useEffect(() => {}, [props.fileUrl]);
@@ -67,6 +84,20 @@ export default function ILovePdfFrameIndex(props: PropsType) {
               <Typography variant="subtitle1">Compress Files</Typography>
               <Typography variant="body2" fontSize={10}>
                 عدد الملفات المحددة : {selectedFilesIds.length} ملفات
+              </Typography>
+            </Stack>
+          </Button>
+        </Grid>
+        <Grid item xs={6} p={2} textAlign={"center"}>
+          <Button
+            onClick={handleDownloadFile}
+            variant="contained"
+            startIcon={<GetAppOutlinedIcon />}
+          >
+            <Stack spacing={1}>
+              <Typography variant="subtitle1">Download File</Typography>
+              <Typography variant="body2" fontSize={10}>
+                عدد التحميلات لملف : {activeFileToShow?.downloaded ?? 0}
               </Typography>
             </Stack>
           </Button>
