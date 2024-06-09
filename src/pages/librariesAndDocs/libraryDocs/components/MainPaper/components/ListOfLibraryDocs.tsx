462831d4513c911f72eb7e329c25c2e0cff48794
Sallam Rady <salamrady@gmail.com>
2024-06-09 11:35:09 +0300

    handle if there is no files

--- src/pages/librariesAndDocs/libraryDocs/components/MainPaper/components/ListOfLibraryDocs.tsx
+++ src/pages/librariesAndDocs/libraryDocs/components/MainPaper/components/ListOfLibraryDocs.tsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useContext, useState } from "react";
 import { LibraryDocumentionContext } from "../../../context/LibraryDocumentionContext";
 import AddEditLibDocDialog from "../../Dialog";
+import { Typography } from "@mui/material";
 
 export default function ListOfLibraryDocs() {
   // TODO::define and declare component state and variables
@@ -16,6 +17,18 @@ export default function ListOfLibraryDocs() {
       {files?.map((ele) => (
         <SingleDoc file={ele} key={ele.id} />
       ))}
+      {files?.length == 0 && (
+        <Stack
+          justifyContent={"center"}
+          alignItems={"center"}
+          width={"100%"}
+          height={200}
+        >
+          <Typography variant="body1" fontSize={20} fontWeight={600}>
+            لا يوجد ملفات فى هذا المجلد
+          </Typography>
+        </Stack>
+      )}
     </Stack>
   );
 }
